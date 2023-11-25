77290da28bd10d0043432c4b447d301a7d870fae
Chaiwut <chivwut0646@gmail.com>
2023-11-25 14:47:19 +0700

    test

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,30 +1,53 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import {reactive} from 'vue'
+
+const gendersList = ['man', 'women', 'LGBTQ']
+const interestslist =['book', 'game', 'movie']
+const formdata = reactive({
+  interest: []
+})
+const submitFrom = () =>{
+  console.log('formdata', formdata)
+}
 </script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <div class="register">
+    <div class="fistname"><div>
+      Firstname
+    </div> 
+      <input type="text" name="firstname" v-model="formdata.firstname">
+    </div>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <div class="lastname">
+      <div>
+        Lastname
+      </div>
+       <input type="text" name="lastname" v-model="formdata.lastname">
+    </div>
+
+    <div class="gender">
+      <div>Gender</div>
+      <div class="gender-for" v-for="gender in gendersList">
+        <input type="radio" name="gender" :value="gender" v-model="formdata.gender"> 
+        {{ gender }}
+      </div>
+    </div>
+
+    <div class="interest">
+        <div>Interest</div>
+        <div class="interest-for" v-for="interest in interestslist">
+          <input type="checkbox" name="interest" :value="interest"
+          v-model="formdata.interest"> 
+        {{ interest }}
+        </div>
+      </div>
+
+    <div class="description">
+      <div>
+        Description
+      </div>
+      <textarea name="texarea" id="textarea" cols="25" rows="2" v-model="formdata.textare"></textarea>
+    </div>
+    <button @click="submitFrom()">submit</button>
+  </div>
+</template>
